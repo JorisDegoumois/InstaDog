646a14bed7ac636f4178908c27783549ffcecbce
ranajarwa <45936064+ranajarwa@users.noreply.github.com>
2019-01-28 16:42:10 +0100

    ces pages que je les ai fini aujaurdĥui

---
+++ main.js
@@ -0,0 +1,11 @@
+$("#demoCustomPalette").mdbWYSIWYG({
+  colorPalette: {
+    red: '#d50000',
+    green: '#64dd17',
+    yellow: '#fff176',
+    blue: '#03a9f4',
+    purple: '#6a1b9a',
+    white: '#fff',
+    black: '#000'
+  }
+});
