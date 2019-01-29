599e5edca97d17a1f5fbe7d3c8f7f37aeb68178c
ranajarwa <45936064+ranajarwa@users.noreply.github.com>
2019-01-29 12:59:03 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-$("#demoCustomPalette").mdbWYSIWYG({
-  colorPalette: {
-    red: '#d50000',
-    green: '#64dd17',
-    yellow: '#fff176',
-    blue: '#03a9f4',
-    purple: '#6a1b9a',
-    white: '#fff',
-    black: '#000'
-  }
-});
+// MDB Lightbox Init
+$(function () {
+  $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+  });
