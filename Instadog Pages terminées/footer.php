fe091d5f32132bcdaf4321e38d74285697a11f79
Unknown <ranajarwa12@gmail.com>
2019-02-01 10:42:26 +0100

    ajout de tout le projet

---
+++ Instadog Pages terminées/footer.php
@@ -0,0 +1,40 @@
+<!-- footer -->
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.0/css/mdb.min.css" rel="stylesheet">
+   <!--  mon ficher css -->
+   <link href ="Instadog.css" rel="stylesheet">
+<div class="footer">
+        <p>S'enregistrer avec :</p>
+        
+        <a type="button" class="light-blue-text mx-2">
+            <i class="fab fa-facebook-f"></i>
+        </a>
+
+        <a type="button" class="light-blue-text mx-2">
+            <i class="fab fa-twitter"></i>
+        </a>
+
+        <a type="button" class="light-blue-text mx-2">
+            <i class="fab fa-linkedin-in"></i>
+        </a>
+
+        <a type="button" class="light-blue-text mx-2">
+            <i class="fab fa-github"></i>
+        </a>
+
+        <div class="footer-copyright text-center py-3">
+            ©Realise 2018:
+            Cr&eacute;er par Rana et Joris
+        </div>
+
+       
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script> 
+</div>
---
+++ SQL/instadog.sql
@@ -0,0 +1,50 @@
+  create database InstaDog; 
+ 
+create user "adminInstadog"@"localhost" identified by "inst@D0g";
+
+grant all privilege ON InstaDog.* to "adminInstadog"@"localhost"
+
+
+
+
+
+
+create table Profile_User(
+    userId int(100) not null primary key,
+    nom varchar (255),
+    prenom varchar(255),
+    password varchar (255),
+    login varchar(25)
+);
+
+create table Chien(
+    chienId int(100) not null primary key,
+    userId int(100),
+    nom varchar (255),
+    race varchar(255),
+    age int(10),
+    surnom varchar(255),
+    elevage varchar(255),
+    photo varchar(255),
+    foreign key (userId) references Profile_User (userId)
+);
+
+create table Article (
+    articleId int (100) not null primary key,
+    chienId int(100),
+    message varchar(255),
+    photo varchar(255),
+    dateParution date,
+    foreign key (chienId)references Chien (chienId)    
+);
+
+create table commentaire (
+    id int(100) not null primary key,
+    userId int(100),
+    articleId int (100),
+    login varchar(25),
+    message varchar (255),
+    date date,
+    foreign key (userId)references Profile_User (userId),
+    foreign key (articleId)references Article (articleId)    
+);
