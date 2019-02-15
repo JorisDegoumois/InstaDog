aafae9271c6e20ca7edd06299cea7cdbae7f47ce
Unknown <ranajarwa12@gmail.com>
2019-02-15 09:22:57 +0100

    logout

--- php/logout.php
+++ php/logout.php
@@ -1,6 +1,6 @@
-<?php 
-session_start();
-
-session_unset();
-session_destroy();
+<?php   
+session_start(); //to ensure you are using same session
+session_destroy(); //destroy the session
+header("location:enregistrer.php"); //to redirect back to "index.php" after logging out
+exit();
 ?>
